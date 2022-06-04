e36fe5ae0091d4916bff26b8a1855d9229d5a738
DevMonster <95547649+EnterKaka@users.noreply.github.com>
2022-06-04 23:10:02 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 import * as React from "react";
 import Account from "./account/";
-import Toolbar from "@mui/material/Toolbar";
+import { Toolbar, Box } from "@mui/material";
+import TelegramIcon from "@mui/icons-material/Telegram";
+
 export default function Header() {
     return (
         <Toolbar sx={{ flexFlow: "row-reverse" }}>
             <Account />
+            <Box
+                sx={{
+                    backgroundColor: "#4d5f6b",
+                    mr: 3,
+                    borderRadius: "50%",
+                    height: "35px",
+                    width: "35px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    cursor: "pointer",
+                }}
+                onClick={() => {
+                    window.open("https://t.me/softdrink1991", "_blank");
+                }}
+            >
+                <TelegramIcon fontSize="medium" />
+            </Box>
         </Toolbar>
     );
 }
